f97dd92c01e51da39ae6072eda82ab8daeccda90
Leo LEBLANC <leo.leblanc@lyceefulbert.fr>
2024-11-13 14:43:13 +0100

    second push

--- AutoFact/Vue/Recapitulatif.Designer.cs
+++ AutoFact/Vue/Recapitulatif.Designer.cs
@@ -28,12 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Recapitulatif";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Recapitulatif));
+            panel2 = new Panel();
+            panelClient = new Panel();
+            buttonRecap = new Button();
+            buttonFact = new Button();
+            buttonPresta = new Button();
+            buttonClient = new Button();
+            pictureBox1 = new PictureBox();
+            panelClient.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(237, 54, 54);
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(210, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(3, 877);
+            panel2.TabIndex = 3;
+            // 
+            // panelClient
+            // 
+            panelClient.BackColor = Color.FromArgb(45, 45, 45);
+            panelClient.Controls.Add(buttonRecap);
+            panelClient.Controls.Add(buttonFact);
+            panelClient.Controls.Add(buttonPresta);
+            panelClient.Controls.Add(buttonClient);
+            panelClient.Controls.Add(pictureBox1);
+            panelClient.Dock = DockStyle.Left;
+            panelClient.Location = new Point(0, 0);
+            panelClient.Name = "panelClient";
+            panelClient.Size = new Size(210, 877);
+            panelClient.TabIndex = 2;
+            // 
+            // buttonRecap
+            // 
+            buttonRecap.FlatAppearance.BorderSize = 0;
+            buttonRecap.FlatStyle = FlatStyle.Flat;
+            buttonRecap.Font = new Font("Segoe UI", 16F, FontStyle.Underline);
+            buttonRecap.ForeColor = SystemColors.Control;
+            buttonRecap.Location = new Point(29, 560);
+            buttonRecap.Name = "buttonRecap";
+            buttonRecap.Size = new Size(147, 46);
+            buttonRecap.TabIndex = 4;
+            buttonRecap.Text = "Récapitulatif";
+            buttonRecap.UseVisualStyleBackColor = true;
+            // 
+            // buttonFact
+            // 
+            buttonFact.BackgroundImageLayout = ImageLayout.None;
+            buttonFact.FlatAppearance.BorderSize = 0;
+            buttonFact.FlatStyle = FlatStyle.Flat;
+            buttonFact.Font = new Font("Segoe UI", 16F, FontStyle.Underline);
+            buttonFact.ForeColor = SystemColors.Control;
+            buttonFact.Location = new Point(29, 434);
+            buttonFact.Name = "buttonFact";
+            buttonFact.Size = new Size(147, 36);
+            buttonFact.TabIndex = 3;
+            buttonFact.Text = "Facture";
+            buttonFact.UseVisualStyleBackColor = true;
+            // 
+            // buttonPresta
+            // 
+            buttonPresta.FlatAppearance.BorderSize = 0;
+            buttonPresta.FlatStyle = FlatStyle.Flat;
+            buttonPresta.Font = new Font("Segoe UI", 16F, FontStyle.Underline);
+            buttonPresta.ForeColor = SystemColors.Control;
+            buttonPresta.Location = new Point(29, 295);
+            buttonPresta.Name = "buttonPresta";
+            buttonPresta.Size = new Size(147, 46);
+            buttonPresta.TabIndex = 2;
+            buttonPresta.Text = "Prestation";
+            buttonPresta.UseVisualStyleBackColor = true;
+            // 
+            // buttonClient
+            // 
+            buttonClient.BackColor = Color.FromArgb(45, 45, 45);
+            buttonClient.FlatAppearance.BorderSize = 0;
+            buttonClient.FlatStyle = FlatStyle.Flat;
+            buttonClient.Font = new Font("Segoe UI", 16F, FontStyle.Underline);
+            buttonClient.ForeColor = SystemColors.Control;
+            buttonClient.Location = new Point(29, 180);
+            buttonClient.Name = "buttonClient";
+            buttonClient.Size = new Size(147, 40);
+            buttonClient.TabIndex = 1;
+            buttonClient.Text = "Client";
+            buttonClient.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(73, 64);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(48, 50);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // Recapitulatif
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(1281, 877);
+            Controls.Add(panel2);
+            Controls.Add(panelClient);
+            Name = "Recapitulatif";
+            Text = "Recapitulatif";
+            panelClient.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel2;
+        private Panel panelClient;
+        private Button buttonRecap;
+        private Button buttonFact;
+        private Button buttonPresta;
+        private Button buttonClient;
+        private PictureBox pictureBox1;
     }
 }
